cc92998125510d65f4db06069335ba3ca14c3544
Luis Enrique Garcia Domador <95010608+luisengardo60@users.noreply.github.com>
2023-10-20 15:12:53 -0400

    nuevo20102023

--- index.php
+++ index.php
@@ -1,113 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>C.A.P.I.</title>
-    <script src="js/jquery-3.4.1.js"></script>
-    <link rel="stylesheet" href="css/estilos.css">
-    <link rel="stylesheet" href="css/cdnjscloudflareAjax.css">
-    <!--<link rel="stylesheet" href="css/sidebar0.css">-->
-    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>C.A.P.I.</title>
+  <!--<link rel="stylesheet" href="../css/bootstrap.min.css">-->
+  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/cdnjscloudflareAjax.css">
+  <script src="js/jquery-3.4.1.js"></script>
 </head>
-<body id="body">
-    
-    
-        <div class="icon__menu">
-            <i class="fas fa-bars" id="btn_open"></i>
-        </div>
-  
 
-    <div class="menu__side" id="menu_side">
+<body>
+  <header>
+    <img src="img/cantvLogo.jpg" title="CANTV" style="background:blue;width:120px; height:120px; float:left;">
+    <img src="img/cantvLogo.jpg" title="CANTV" style="background:blue;width:120px; height:120px; float:right;">
 
-        <div class="name__page">
-            <i><img src="img/menu_inicio6.jpg" alt="C.A.P.I" style="border-radius: 50px; float:right; width: 40px; height: 40px;"></i>
-            <h4><a href="../inicio.php" style="color:white;">inicio</a></h4>
-        </div>
-
-        <div class="options__menu">	
-
-            <a href="#">
-                <div class="option">
-                    <!--<i title="Organigrama" style="font-size:30px">&#128200;</i>-->
-                    <h4><b>Organigrama</b</h4>
-                </div>
-            </a>
-
-            <a href="#">
-                <div class="option">
-                    <i title="Servidores" style="font-size:30px">&#128187;</i>
-                    <h4><b>Servidores</b></h4>
-                </div>
-            </a>
-            
-            <a href="#">
-                <div class="option">
-                    <i class="fas fa-video" title="Cursos"></i>
-                    <h4><b>Diapositivas</b></h4>
-                </div>
-            </a>
-
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-sticky-note" title="Blog"></i>
-                    <h4><b>Check List Aplicativos CAPI</b></h4>
-                </div>
-            </a>
-
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-id-badge" title="Contacto"></i>
-                    <h4><b>Modulo Kenan SAecurity (KUS)</b></h4>
-                </div>
-            </a>
-
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-address-card" title="Nosotros"></i>
-                    <h4><b>Ciclos de Facturación</b></h4>
-                </div>
-            </a>
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-address-card" title="Nosotros"></i>
-                    <h4><b>Ciclos de Facturación</b></h4>
-                </div>
-            </a>
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-address-card" title="Nosotros"></i>
-                    <h4><b>Ciclos de Facturación</b></h4>
-                </div>
-            </a>
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-address-card" title="Nosotros"></i>
-                    <h4><b>Ciclos de Facturación</b></h4>
-                </div>
-            </a>
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-address-card" title="Nosotros"></i>
-                    <h4><b>Ciclos de Facturación</b></h4>
-                </div>
-            </a>
-
-        </div>
-
-    </div>
-   <main>
-   <?php include '../LabCapi/titulo/titulo.html' ?>
-
-        <h1 class="sombra3" style="color:aqua"><b>ADMINFACT</b></h1><br><br><br><br><br><br><br>
-
-    
-    <?php include '../LabCapi/reloj/reloj1.html' ?>
-  
-</main>
+  </header>
+  <nav>
+    <?php include 'titulo/titulo.html' ?>
 
 
-    <script src="js/script.js"></script>
+  </nav><br><br><br>
+
+  <a href="inicio.php"><button class="boton"><b>Entrar</b></button></a>
+
+
+
+  <article>
+    <?php include 'reloj/reloj.html' ?>
+  </article>
 </body>
+
 </html>
---
+++ inicio.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>C.A.P.I.</title>
+    <!--<link rel="stylesheet" href="../css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/cdnjscloudflareAjax.css">
+    <script src="../js/jquery-3.4.1.js"></script>
+</head>
+<!--<body style="background: darkblue;">-->
+<body style="background: url(img/tierra.jpg) no-repeat center center fixed; background-size:cover;">-->
+    <header>
+        <img src="img/cantvLogo.jpg" title="CANTV" style="background:blue;width:120px; height:120px; float:left;">
+        <img src="img/cantvLogo.jpg" title="CANTV" style="background:blue;width:120px; height:120px; float:right;">
+
+    </header>
+
+    <?php include 'titulo/titulo1.html' ?>
+
+    <br><br><br><br>
+    <div class="ini">
+        <a href="indexAdminfact.php"><button class="boton"><b>ADMINFACT</b></button></a>
+
+
+        <a href="html/indexAdminBasis.php"><button class="boton"><b>ADMINBASIS</b></button></a>
+
+        <a href="html/indexAdminCrm.php"><button class="boton"><b>ADMINCRM</b></button></a>
+
+        <a href="html/indexAdminEdi.php"><button class="boton"><b>ADMINEDI</b></button></a>
+
+        <a href="html/indexProyectos.php"><button class="boton"><b>PROYECTOS</b></button></a>
+    </div>
+
+
+    <div class="ini2">
+        <a href="html/indexRecaudacion.php"><button class="boton"><b>FACTURACIÓN</b></button></a>
+
+        <a href="html/indexEmpresarial.php"><button class="boton"><b>EMPRESARIAL</b></button></a>
+
+        <a href="html/indexAprovision.php"><button class="boton"><b>APROVISIONAMIENTO</b></button></a>
+        <a href="html/indexTecnologia.php"><button class="boton"><b>TECNOLOGÌA</b></button></a>
+    </div>
+
+    <div class="ini3">
+        <a href="http://www.google.com.ve"><button class="boton"><b style="color:darkblue">SALIR</b></button></a>
+    </div>
+
+
+        <?php include 'reloj/reloj.html' ?>
+
+</body>
+
+</html>
