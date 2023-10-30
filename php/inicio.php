0c08e23b2b355e6d64c62534ea6f07806a11eeac
Luis Enrique Garcia Domador <95010608+luisengardo60@users.noreply.github.com>
2023-10-30 19:49:34 -0400

    lun30102023

--- php/inicio.php
+++ php/inicio.php
@@ -18,11 +18,52 @@
         <img src="../img/cantvLogo.jpg" title="CANTV" style="background:blue;width:120px; height:120px; float:right;">-->
 
     </header>
-
+    
     <?php include '../titulo/titulo1.html' ?>
+    <br><br>
+
+    <ul class="hList">
+        <li>
+            <a href="#click" class="menu">
+                <h2 class="menu-title">Admin</h2>         
+                
+                <ul class="menu-dropdown">
+                    <li><a href="indexAdminfact.php">Adminfact
+                    <li><a href="indexAdminfact.php">Adminfact
+                    <li><a href="indexAdminBasis.php">Adminbasis
+                    <li><a href="indexAdminCrm.php">Admincrm
+                    <li><a href="indexAdminEdi.php">Adminedi
+                    </a></a> </a></a></a>
+            </li></li></li></li></li>
+
+
+                </ul>
+           
+          
+        <li>
+            <a href="#click" class="menu">
+                <h2 class="menu-title menu-title_2nd">Aplicaciones</h2>
+                <ul class="menu-dropdown">
+                    <li>Proyectos</li>
+                    <li>Facturación</li>
+                    <li>Empresarial</li>
+                    <li>Aprovisionamiento</li>
+                    <li>Tecnología</li>
+                </ul>
+            </a>
+        </li>
+        <li>
+            <a href="#click" class="menu">
+                <h2 class="menu-title menu-title_3rd">Salir</h2>
+                <ul class="menu-dropdown">
+                    <li>Salir</li>
+                </ul>
+            </a>
+        </li>
+    </ul>
 
     
-    <div class="ini">
+   <!-- <div class="ini">
         <a href="indexAdminfact.php"><button class="boton"><b>ADMINFACT</b></button></a>
 
 
@@ -33,10 +74,10 @@
         <a href="indexAdminEdi.php"><button class="boton"><b>ADMINEDI</b></button></a>
 
         <a href="indexProyectos.php"><button class="boton"><b>PROYECTOS</b></button></a>
-    </div>
+    </div>-->
 
 
-    <div class="ini2">
+   <!-- <div class="ini2">
         <a href="indexRecaudacion.php"><button class="boton"><b>FACTURACIÓN</b></button></a>
 
         <a href="indexEmpresarial.php"><button class="boton"><b>EMPRESARIAL</b></button></a>
@@ -47,7 +88,7 @@
 
     <div class="ini3">
         <a href="http://www.google.com.ve"><button class="boton"><b>SALIR</b></button></a>
-    </div>
+    </div>-->
 
 
         <?php include '../reloj/reloj1.html' ?>
