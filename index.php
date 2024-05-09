0f2723c5f836fd10e6ee4f75e631771d11be651b
Luis Enrique Garcia Domador <95010608+luisengardo60@users.noreply.github.com>
2024-05-08 20:43:07 -0400

    dia08052024

--- index.php
+++ index.php
@@ -25,8 +25,7 @@
             <div class="bg"></div>
             
             <div class="body__card_front">
-              <h1 style="color:white">Entrar</h1>
-            </div>
+                          </div>
           </div>
           <div class="card__back">
 
