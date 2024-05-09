50a68b345111ce27bbab4ee79775cf72f50a33a7
Luis Enrique Garcia Domador <95010608+luisengardo60@users.noreply.github.com>
2024-05-08 21:36:39 -0400

    diaC08052024

--- php/inicio.php
+++ php/inicio.php
@@ -25,121 +25,11 @@
 			</nav>
 		</div>
 	</header>
-
-<body>
-    <div class="container__card">
-
-        <div class="card__father">
-            <div class="card">
-
-                <div class="card__front">
-                    <div class="bg">
-                        <h2>CANTV<BR>Portales</h2>
-                    </div>
-
-
-                </div>
-                <!--   <div class="card__back" style="background: url(../img/venezuela1.jpg) no-repeat center center fixed; background-size: cover;">-->
-                <div class="card__back">
-                    <div class="body__card_back">
-                        <!--<h1>Entrar</h1>-->
-                        <p style="color:white;">Bienvenidos</p><br><br>
-                        <a href="../php/indexAdminPortales.php"><button class="boton"><b>Portales</b></button></a>
-                        <!--<a href="tel:+584129885610">Aquí el texto que quieras</a>--> <!--<input><a href="indexAdminfact.php"><button class="boton"><b>Entrar</b></button></a> value="Leer Más">-->
-                        <br><br>
-                        <!--<h3>C.A.P.I.</h3>-->
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-        <div class="card__father">
-            <div class="card">
-                <div class="card__front">
-                    <div class="bg">
-                        <h2>Admin</h2>
-                    </div>
-
-                    <div class="body__card_front">
-                        <!-- <h1>C.A.P.I</h1>-->
-                    </div>
-                </div>
-                <div class="card__back">
-                    <div class="body__card_back">
-                        <!--<h1>Entrar</h1>-->
-                        <!--<p>Bienvenidos</p>-->
-                        <a href="../php/indexAdminfact.php"><button class="boton"><b>AdminFact</b></button></a>
-                        <a href="../php/indexAdminBasis.php"><button class="boton"><b>AdminBasiss</b></button></a>
-                        <a href="../php/indexAdminCrm.php"><button class="boton"><b>AdminCrm</b></button></a>
-                        <a href="../php/indexAdminEdi.php"><button class="boton"><b>AdminEdi</b></button></a>
-                        <!--<input><a href="php/inicio.php"><button class="boton"><b>Entrar</b></button></a> value="Leer Más">-->
-
-                        <!--<h3>ADMIN</h3>-->
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-        <div class="card__father">
-            <div class="card">
-                <div class="card__front">
-                    <div class="bg">
-                        <h2>Aplicaciones
-                        </h2>
-                    </div>
-
-                    <div class="body__card_front">
-                        <!-- <h1>C.A.P.I</h1>-->
-                    </div>
-                </div>
-                <div class="card__back">
-
-                    <div class="body__card_back">
-                        <!--<h1>Entrar</h1>-->
-                        <!--<p>Bienvenidos</p>-->
-                        <a href="indexProyectos.php"><button class="boton"><b>Proyectos</b></button></a>
-                        <a href="indexRecaudacion.php"><button class="boton"><b>Facturación</b></button></a>
-                        <a href="indexEmpresarial.php"><button class="boton"><b>Empresarial</b></button></a>
-                        <a href="indexAprovision.php"><button class="boton"><b>Aprovisionamiento</b></button></a>
-                        <a href="indexTecnologia.php"><button class="boton"><b>Tecnologia</b></button></a>
-                        <!--<input><a href="php/inicio.php"><button class="boton"><b>Entrar</b></button></a> value="Leer Más">-->
-                        <br><br>
-                        <!-- <h3>APLICACIONES</h3>-->
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-        <div class="card__father">
-            <div class="card">
-                <div class="card__front">
-                    <div class="bg">
-                        <h2>Salir</h2>
-                    </div>
-
-                    <div class="body__card_front">
-                        <!-- <h1>C.A.P.I</h1>-->
-                    </div>
-                </div>
-                <div class="card__back">
-
-                    <div class="body__card_back">
-                        <!--<h1>Entrar</h1>-->
-                        <b style="color:white;">Hasta Pronto</b><br><br>
-                        <a href="https://www.google.com"><button class="boton"><b>Salir</b></button></a>
-                        <!--<input><a href="php/inicio.php"><button class="boton"><b>Entrar</b></button></a> value="Leer Más">-->
-                        <br><br>
-                        <!--<h3>SALIR</h3>-->
-                    </div>
-
-                </div>
-            </div>
-        </div>
+    <div class="icon__menu">
+        <i class="fas fa-bars" id="btn_open"><a class="sombra3" style="color:cyan">&nbsp;C.A.P.I.</a></i>
+        <!--<span>&Congruent;</span>-->
     </div>
-
+    </div>
     <nav class="sidebar">
         <div class="menu__side" id="menu_side">
             <ul>
@@ -156,10 +46,10 @@
                 </li>
                 <li><a href="#" class="serv-btn">Aplicaciones<span class="second">&#x021D3;</span></a>
                     <ul class="serv-show">
+                        <li><a href="../php/indexAprovision.php">Aprovisionamiento</a></li>
+                        <li><a href="../php/indexEmpresarial.php">Empresarial</a></li>
                         <li><a href="../php/indexProyectos.php">Proyectos</a></li>
                         <li><a href="../php/indexRecaudacion.php">Facturación</a></li>
-                        <li><a href="../php/indexEmpresarial.php">Empresarial</a></li>
-                        <li><a href="../php/indexAprovision.php">Aprovisionamiento</a></li>
                         <li><a href="../php/indexTecnologia.php">Tecnologia</a></li>
                     </ul>
                 </li>
@@ -169,9 +59,7 @@
 
 
     <main>
-        
-
-        <!--<h1 class="sombra3" style="color:cyan"><b>PORTALES</b></h1>--><br><br><br><br><br><br><br><br><br><br>
+       <br><br><br><br><br><br><br><br><br><br>
         
 
         <?php include '../reloj/reloj1.html' ?>
@@ -183,3 +71,5 @@
 </body>
 
 </html>
+   
+   
