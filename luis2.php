1092b7b88a5e73276d550947616df0254efbc487
Luis Enrique Garcia Domador <95010608+luisengardo60@users.noreply.github.com>
2024-07-03 13:53:42 -0400

    D03072024

---
+++ luis2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CAPI</title>
+    <link rel="stylesheet" href="css/imagen.css">
+    <!--<link rel="stylesheet" href="css/index.css">-->
+    <link rel="stylesheet" href="css/cdnjscloudflareAjax.css">
+    <script src="js/jquery-3.4.1.js"></script>
+</head>
+
+<body>
+    <div>
+        <div>
+            <?php include 'titulo/titulo1.html' ?>
+
+            <div class="container">
+                <div class="logo"></div><a href="php/inicio.php"></a>
+                <br><br><br><br><br><br><br><br><br><br>
+
+                <br></br><?php include 'reloj/reloj.html' ?>
+                <h3 style="color:white" ;>Bienvenidos</h3>
+                <a href="php/inicio.php"><button class="boton"><h3><i>Entrar</i></h3></button></a>
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
